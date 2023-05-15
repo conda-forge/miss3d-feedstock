9e3655e8bef1ddd7e49b505de34e26e4794461a3
Luca Dall'Olio <luca.dallolio@aego.ai>
2023-05-15 07:20:03 +0200

    adding visual output for better debugging

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,12 @@ echo "**************** M I S S 3 D  B U I L D  S T A R T S  H E R E ************
 
 cp src/Makefile.inc.gnu64 src/Makefile.inc
 
+echo "MAKEFILE : "
+cat ./Makefile
+
+echo "MAKEFILE.INC : "
+cat ./src/Makefile.inc
+
 make prefix=$PREFIX/bin
 make install
 
