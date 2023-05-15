2d43eed2c2e5a0e2bbe3b539a6aa3c5a795dcf6d
Luca Dall'Olio <luca.dallolio@aego.ai>
2023-05-15 21:25:23 +0200

    using FC for compiler

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,13 +5,7 @@ echo "**************** M I S S 3 D  B U I L D  S T A R T S  H E R E ************
 
 cp src/Makefile.inc.gnu64 src/Makefile.inc
 
-echo "MAKEFILE : "
-cat ./Makefile
-
-echo "MAKEFILE.INC : "
-cat ./src/Makefile.inc
-
-make VERBOSE=1 prefix=$PREFIX/bin F90=$F90
+make VERBOSE=1 prefix=$PREFIX/bin F90=$FC  
 make install
 
 echo "**************** M I S S 3 D  B U I L D  E N D S  H E R E ****************"
