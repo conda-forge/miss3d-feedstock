9fd90fd4407159d49baae00371943c6d5164afc9
Luca Dall'Olio <luca.dallolio@aego.ai>
2023-05-16 11:57:04 +0200

    tentative fix for mingw32-make executable

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,8 +5,8 @@ rem set "LIB=%LIBRARY_LIB%;%LIB%"
 
 copy src\Makefile.inc.gnu64 src\Makefile.inc
 
-mingw64-make prefix=%PREFIX%/bin F90=%fortran_compiler%
+mingw32-make prefix=%PREFIX%/bin F90=%fortran_compiler%
 if errorlevel 1 exit 1
 
-mingw64-make install
+mingw32-make install
 if errorlevel 1 exit 1
