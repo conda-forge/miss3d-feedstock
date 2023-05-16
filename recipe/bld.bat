bc0b869c936ca2c259c09387e796793dc738c9d1
Luca Dall'Olio <luca.dallolio@aego.ai>
2023-05-16 10:55:04 +0200

    switching to m2w64-make

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,8 +5,8 @@ rem set "LIB=%LIBRARY_LIB%;%LIB%"
 
 copy src\Makefile.inc.gnu64 src\Makefile.inc
 
-nmake prefix=%PREFIX%/bin F90=%fortran_compiler%
+m2w64-make prefix=%PREFIX%/bin F90=%fortran_compiler%
 if errorlevel 1 exit 1
 
-nmake install
+m2w64-make install
 if errorlevel 1 exit 1
